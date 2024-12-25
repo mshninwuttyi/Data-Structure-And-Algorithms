f0bd9d77fed280d08e4d03d91ffcf51a24403ca7
Hnin Wutt Yi <ms.hninwuttyi@gmail.com>
2024-12-25 22:37:12 +0700

    feat: Add Linear Search Algorithm

--- DSA-Project/src/main/java/com/hwy/dsa/LinearSearch.java
+++ DSA-Project/src/main/java/com/hwy/dsa/LinearSearch.java
@@ -2,30 +2,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package LinearSearch;
+package com.hwy.dsa;
 
 /**
  *
  * @author LENOVO
  */
-public class LinearSearch() {
-    public int Search(int[]array, int item)
+public class LinearSearch {
+    public int search(int[]array,int item)
     {
-        for(int i=0; i<array.length; i++)
-        {
-            if(array[i] == item)
+        for (int i = 0; i < array.length; i++) {
+            if(array[i]==item)
             {
                 return i;
             }
         }
         return -1;
     }
-    
-    public static void main(string[]args)
+    public static void main(String[]args)
     {
         int[] input = {1,2,3,4,10};
         LinearSearch ls = new LinearSearch();
-        System.out.println("Search "+ ls.Search(input,100));
+        System.out.println("Search "+ ls.search(input, 100));
     }
-    
 }
---
+++ DSA-Project/src/main/java/com/hwy/dsa/search/BinarySearchAlgorithm.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hwy.dsa.search;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class BinarySearchAlgorithm {
+    
+}
