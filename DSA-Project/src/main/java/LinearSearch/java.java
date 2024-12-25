a25f47d59aa35eb55bac4a134de5fa48c741724c
Hnin Wutt Yi <ms.hninwuttyi@gmail.com>
2024-12-25 19:53:34 +0700

    Create a DSA-Project

---
+++ DSA-Project/src/main/java/LinearSearch/java.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LinearSearch;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class LinearSearch() {
+    public int Search(int[]array, int item)
+    {
+        for(int i=0; i<array.length; i++)
+        {
+            if(array[i] == item)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    public static void main(string[]args)
+    {
+        int[] input = {1,2,3,4,10};
+        LinearSearch ls = new LinearSearch();
+        System.out.println("Search "+ ls.Search(input,100));
+    }
+    
+}
