9948202266194c70b0c08f84ee3964ee04a33caf
Hnin Wutt Yi <ms.hninwuttyi@gmail.com>
2024-12-25 23:39:55 +0700

    feat: Add BinarySearchAlgorithm

--- DSA-Project/src/main/java/com/hwy/dsa/search/BinarySearchAlgorithm.java
+++ DSA-Project/src/main/java/com/hwy/dsa/search/BinarySearchAlgorithm.java
@@ -1,13 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.hwy.dsa.search;
 
-/**
- *
- * @author LENOVO
- */
-public class BinarySearchAlgorithm {
-    
+import com.hwy.dsa.searching.SearchAlgorithm;
+
+public class BinarySearchAlgorithm implements SearchAlgorithm {
+    @Override
+    public int Search(int[]arr, int item)
+    {
+        int start = 0;
+        int end = arr.length - 1;
+        
+        while(start <= end)
+        {
+            int middle = (start + end)/2;
+            if(arr[middle] == item)
+            {
+                return middle;
+            }
+            else if(arr[middle] > item)
+            {
+                end = middle - 1;
+            }
+            else if(arr[middle] < item)
+            {
+                start = middle + 1;
+            }
+        }
+        return -1;
+    }
 }
