ff524e8e22998efb24bba32a1d76c9e3dffe3b36
Hnin Wutt Yi <ms.hninwuttyi@gmail.com>
2024-12-30 23:46:49 +0700

    feat: Add test file for LinearSearch

---
+++ DSA-Project/src/test/java/com/hwy/dsa/search/LinearTestSearch.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hwy.dsa.search;
+
+import com.hwy.dsa.LinearSearch;
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+public class LinearTestSearch {
+    LinearSearch isAlgo = new LinearSearch();
+    int[]input = {1,2,3,4,10,-1};
+    
+    
+    @parameterizedTest
+    @CsvSource(value = {
+            "1,0",
+            "120,-1",
+            "-1,5"})
+    
+    public void testSearchWithParameterizedTest(int item, int expected)
+    {
+        System.out.println("Item" +item+ " Expected " +expected);
+        
+        int index = isAlgo.Search(input, item);
+        assertEquals(expected, index);
+    }
+}
