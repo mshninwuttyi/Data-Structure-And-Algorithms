c053371486f28fcb49289c8339290e621e89aa49
Hnin Wutt Yi <ms.hninwuttyi@gmail.com>
2025-01-06 23:03:38 +0700

    feat: Add test case for empty array

--- DSA-Project/src/test/java/com/turing/dsa/sorting/SortingTest.java
+++ DSA-Project/src/test/java/com/turing/dsa/sorting/SortingTest.java
@@ -4,6 +4,8 @@ package com.turing.dsa.sorting;
 import com.hwy.dsa.sorting.BubbleSort;
 import com.hwy.dsa.sorting.SortingAlgorithm;
 import org.junit.jupiter.api.Test;
+import com.hwy.dsa.util.Util;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SortingTest {
     Util util = new Util();
