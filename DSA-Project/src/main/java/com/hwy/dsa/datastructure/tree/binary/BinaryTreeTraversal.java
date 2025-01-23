f0cceb024b2b302caa5414ccd2caf9c6dd788650
Hnin Wutt Yi <ms.hninwuttyi@gmail.com>
2025-01-24 00:01:04 +0700

    feat: Add BinaryTreeTraversal interface

---
+++ DSA-Project/src/main/java/com/hwy/dsa/datastructure/tree/binary/BinaryTreeTraversal.java
@@ -0,0 +1,9 @@
+
+package com.hwy.dsa.datastructure.tree.binary;
+
+import org.w3c.dom.Node;
+
+
+public interface BinaryTreeTraversal extends Iterable<Node> {
+   public Iterator<Node> iterator();
+}
